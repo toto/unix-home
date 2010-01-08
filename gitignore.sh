a6e6c798f4805cbbe8fcddefe8be1ec03d199a34
Thomas Kollbach <thomas@kollba.ch>
2010-01-08 21:56:52 +0100

    add gitignore again

---
+++ gitignore.sh
@@ -0,0 +1,32 @@
+[user]
+	name = Thomas Kollbach
+	email = thomas@kollba.ch
+[core]
+  editor = mate -w
+	excludesfile = /Users/toto/.gitignore
+[apply]
+	whitespace = nowarn
+[alias]
+  co = checkout
+  st = status
+[color]
+  branch = auto
+  diff = auto
+  status = auto
+[color "branch"]
+  current = yellow reverse
+  local = yellow
+  remote = green
+[color "diff"]
+  meta = yellow bold
+  frag = magenta bold
+  old = red bold
+  new = green bold
+[color "status"]
+  added = yellow
+  changed = green
+  untracked = cyan
+[push]
+	default = current
+[github]
+	user = toto
